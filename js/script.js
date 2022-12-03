bd423bde2df483f103e2209deb29c891a4fe44db
shwetamisar <62926371+shwetamisar@users.noreply.github.com>
2022-12-03 19:48:40 +0530

    Added eventListener to listen to the click event

--- js/script.js
+++ js/script.js
@@ -4,5 +4,26 @@ const checkButton  =  document.querySelector("#check-button");
 const message      =  document.querySelector("#error-message");
 const noOfNotesRow=document.querySelectorAll('.no-of-notes')
 
+const availableNotes = [2000,500,100,20,10,5,1];
 
+checkButton.addEventListener("click", validateCashAndBillAmount)
+function validateCashAndBillAmount(){
+    const cashAmt = parseInt(cashGiven.value);
+    const billAmt = parseInt(billAmount.value);
+
+    if(billAmt > 0){
+        if(cashAmt >= billAmt){
+            const cashToBeReturned = cashAmt-billAmt;
+            calculateChange(cashToBeReturned);
+        }
+        else{
+            message.innerText = "Do you wanna wash plates?"
+        }
+
+    }
+    else{
+        message.innerText = "Invalid Bill Amount";
+        message.style.color = "Red";
+    }
+}
 
