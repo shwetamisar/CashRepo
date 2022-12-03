694e9caec4f56d786650a816d4c2f85438e4cfd0
shwetamisar <62926371+shwetamisar@users.noreply.github.com>
2022-12-03 19:45:38 +0530

    Taking html inputs to the js file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const billAmount   =  document.querySelector("#bill-amount");
+const cashGiven    =  document.querySelector("#cash-given");
+const checkButton  =  document.querySelector("#check-button");
+const message      =  document.querySelector("#error-message");
+const noOfNotesRow=document.querySelectorAll('.no-of-notes')
+
+
+
